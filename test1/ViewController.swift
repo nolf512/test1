77cf8748b8ad87024a52a6ad91163828079b5bea
nofprog <k.3217@gmail.com>
2021-11-25 23:10:12 +0900

    add label

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureLabel()
         calcPlus(num1: 10, num2: 20)
         
     }
@@ -19,7 +22,18 @@ class ViewController: UIViewController {
     func calcPlus(num1: Int, num2: Int) -> Int {
         return num1 + num2
     }
+    
+    func configureLabel() {
+        view.addSubview(label)
+        label.text = "Hello"
+        setLabel()
+    }
 
 
+    func setLabel() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
 
